6cb4caa066428f0b5a3bc6f53f90cb9bf2a28ca7
omega-coder <fy_cherief@esi.dz>
2019-04-05 01:54:24 +0200

    Adds session variable

--- Natas/level19/solve.py
+++ Natas/level19/solve.py
@@ -7,7 +7,11 @@ import requests
 
 URL = "http://natas19.natas.labs.overthewire.org"
 cookies = {'PHPSESSID': ''}
+session = requests.Session()
+
+
+
 
 for i in range(1, 641):
     cookies['PHPSESSID'] = '{}-admin'.format(i).encode().hex()
-
+    res = 
